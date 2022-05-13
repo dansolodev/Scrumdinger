dcf1922782434dc1015bde9db973c0597d138af1
Daniel Cinto <dansolo.cc@gmail.com>
2022-05-12 19:29:48 -0500

    Using stacks to arranghe views

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Daniel Cinto Conde on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
